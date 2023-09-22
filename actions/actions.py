45d3345c64ee18e7611f1f678a4c3edb68f48f52
dendyandra22 <dendyandra@gmail.com>
2023-09-22 08:53:29 +0700

    create slot validator

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,39 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.types import DomainDict
+
+class ValidateServiceForm(FormValidationAction):
+    def name(self)-> Text:
+        return "validate_service_form"
+
+    def validate_service(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict)-> Dict[Text, Any]:
+        '''Validate service values'''
+        # if tracker.get_intent_of_latest_message != "give_information":
+        #     dispatcher.utter_message
+
+        if slot_value.lower() not in ['livebot', 'nabot']:
+            dispatcher.utter_message(text="Service name is invalid! We only offer LiveBot and NaBot")
+            return {'service':None}
+
+        return {'service':slot_value}
+    
+
+    def validate_payment(self, slot_value: Any, dispatcher: CollectingDispatcher, tracker: Tracker, domain: DomainDict)-> Dict[Text, Any]:
+        '''Validate payment values'''
+        # if tracker.get_intent_of_latest_message != "give_information":
+        #     dispatcher.utter_message
+
+        if slot_value.lower() not in ['debit card', 'credit card', 'bitcoin', 'debit', 'credit']:
+            dispatcher.utter_message(text="Payment method is invalid! We only accept Debit card, Credit card, and Bitcoin")
+            return {'payment':None}
+
+        return {'payment':slot_value}
+
+
+        
